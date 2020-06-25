375ef6318b937a50a01cad7a0de31269671a8492
prayogo17 <nugrohoprayogo97@gmail.com>
2020-06-25 19:29:23 +0700

    Update eksekusi.php

--- eksekusi.php
+++ eksekusi.php
@@ -3,16 +3,16 @@ require 'PHPMailer/src/PHPMailer.php' ;
 require 'PHPMailer/src/SMTP.php';
 require 'PHPMailer/src/Exception.php';
 
-$koneksi = mysqli_connect("localhost", "root", "", "test");
+$koneksi = mysqli_connect("localhost", "root", "", "test"); //memulai koneksi ke database
 // Cek koneksi
 if (mysqli_connect_errno()) {
     echo "Koneksi database gagal : " . mysqli_connect_error();
 }
 $result = mysqli_query($koneksi, "SELECT * From email");
-// Mengambil semua data email
+// Mengambil semua data email dalam bentuk array
 $result = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-foreach ($result as $key => $value) {
+foreach ($result as $key => $value) { //mengirim email untuk setiap baris data
     $mail =  new PHPMailer\PHPMailer\PHPMailer();
     $mail->IsSMTP();
     $mail->IsHTML(true);
